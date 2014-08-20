559461914867361fb216fe92d91dd3e69ca25794
Maxim Colls <collsmaxim@gmail.com>
2014-08-20 18:41:52 +0200

    Fixed exposed global var name

--- cronut-factory.js
+++ cronut-factory.js
@@ -2,7 +2,7 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['raphael'], function (b) {
-      return (root.returnExportsGlobal = factory(b));
+      return (root.CronutFactory = factory(b));
     });
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
@@ -11,7 +11,7 @@
     module.exports = factory(require('raphael'));
   } else {
     // Browser globals
-    root.returnExportsGlobal = factory(root.Raphael);
+    root.CronutFactory = factory(root.Raphael);
   }
 }(this, function (Raphael) {
   var CronutFactory = {};
